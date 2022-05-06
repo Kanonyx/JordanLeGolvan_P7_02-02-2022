8686758d4cb29c54034d19400b8ae16896e9c94f
Kanonyx <jordan.legolvan@gmail.com>
2022-05-06 15:49:04 +0200

    minor change

--- front/src/components/PostsVue.vue
+++ front/src/components/PostsVue.vue
@@ -9,25 +9,18 @@
                 <v-card elevation="8" width="750">
                     <v-img elevation="8" v-if="post.imageUrl" height="400px" :src="post.imageUrl">
                         <v-card-title class="font-weight-light white--text ">
-                            <v-avatar  size="69">
-                                <img height="400px" class="customcursor" alt="user" :src="post.User.avatar"
-                                    @click="profil(post.UserId)" />
-                            </v-avatar>
-                            <p class="mt-3 ml-3" @click="profil(post.UserId)"> {{
-                                    post.User.first_name
-                            }}</p>
 
                             <v-spacer></v-spacer>
-                            <v-app-bar mb-2 flat color="rgba(0, 0, 0, 0)">
-                                <v-toolbar-title class="text-subtitle-2 font-weight-light white--text pl-15 mr-n15">{{
-                                        dateParser(post.createdAt)
-                                }}</v-toolbar-title>
+                            <v-card mb-2 flat class="text-right justify-end align-end" color="transparent">
+                                <v-toolbar-title
+                                    class="text-subtitle-2 align-end font-weight-light white--text justify-end align-end ">
+                                    {{ dateParser(post.createdAt) }}
+                                </v-toolbar-title>
 
                                 <v-col cols="2" class="pa-0 d-flex text-center justify-end align-end"
                                     v-if="post.UserId == UserId || userAdmin === true">
-
                                 </v-col>
-                            </v-app-bar>
+                            </v-card>
                             <div class="text-center">
                                 <v-menu left bottom>
                                     <template v-slot:activator="{ on, attrs }">
@@ -38,22 +31,32 @@
 
                                     <v-list>
                                         <v-list-item>
-                                            <v-btn color="primary" class="mx-3" @click="messageToEdit(post)"
-                                                v-if="post.UserId == UserId">Editer</v-btn>
-                                            <v-btn color="error" class="mx-3" @click="deleteMessage(post.id)">
-                                                Supprimer</v-btn>
+                                            <v-btn color="white" class="mx-3" @click="messageToEdit(post)"
+                                                    v-if="post.UserId == UserId"><v-icon color="primary">mdi-pencil-outline</v-icon></v-btn>
+                                                <v-btn color="white" class="mx-3" @click="deleteMessage(post.id)">
+                                                    <v-icon  color="red">mdi-delete-alert</v-icon></v-btn>
                                         </v-list-item>
                                     </v-list>
                                 </v-menu>
                             </div>
                         </v-card-title>
                         <v-spacer></v-spacer>
+                        <div class="d-flex ml-3 mt-n4">
+                            <v-avatar size="69">
+                                <img class="customcursor" alt="user" :src="post.User.avatar"
+                                    @click="profil(post.UserId)" />
+                            </v-avatar>
+                            <p class="mt-5 ml-3 font-weight-light white--text" @click="profil(post.UserId)"> {{
+                                    post.User.first_name
+                            }}
+                            </p>
+                        </div>
 
 
                     </v-img>
-                    <v-img v-else height="200px" src="">
-                        <v-app-bar flat color="rgba(0, 0, 0, 0)">
-                            <v-toolbar-title class="text-subtitle-2 white--text pl-0">Posté le : {{
+                    <v-img v-else height="160px" src="@/assets/luke-chesser-IGtutkXikuc-unsplash.jpg">
+                        <v-app-bar>
+                            <v-toolbar-title class="text-subtitle-2 font-weight-light  pl-0"> {{
                                     dateParser(post.createdAt)
                             }}</v-toolbar-title>
                             <v-spacer></v-spacer>
@@ -63,41 +66,41 @@
                                     <v-menu left bottom>
                                         <template v-slot:activator="{ on, attrs }">
                                             <v-btn icon v-bind="attrs" v-on="on">
-                                                <v-icon color="white">mdi-dots-vertical</v-icon>
+                                                <v-icon color="dark">mdi-dots-vertical</v-icon>
                                             </v-btn>
                                         </template>
                                         <v-list>
                                             <v-list-item>
-                                                <v-btn color="primary" class="mx-3" @click="messageToEdit(post)"
-                                                    v-if="post.UserId == UserId">Editer</v-btn>
-                                                <v-btn color="error" class="mx-3" @click="deleteMessage(post.id)">
-                                                    Supprimer</v-btn>
+                                                <v-btn color="white" class="mx-3" @click="messageToEdit(post)"
+                                                    v-if="post.UserId == UserId"><v-icon color="primary">mdi-pencil-outline</v-icon></v-btn>
+                                                <v-btn color="white" class="mx-3" @click="deleteMessage(post.id)">
+                                                    <v-icon  color="red">mdi-delete-alert</v-icon></v-btn>
                                             </v-list-item>
                                         </v-list>
                                     </v-menu>
                                 </div>
                             </v-col>
                         </v-app-bar>
-                        <v-card-title class="white--text mt-8">
-                            <v-avatar size="56">
+                        <v-card-title class="white--text ">
+                            <v-avatar size="66">
                                 <img class="customcursor" alt="user" :src="post.User.avatar"
                                     @click="profil(post.UserId)" />
                             </v-avatar>
-                            <p class="ml-3" @click="profil(post.UserId)">{{ post.User.last_name }} {{
+                            <p class="ml-3 mt-3 font-weight-light" @click="profil(post.UserId)"> {{
                                     post.User.first_name
                             }}</p>
                         </v-card-title>
                     </v-img>
                     <v-card-text>
-                        
-                        <div class="font-weight-bold ml-8 mb-2">{{ post.User.first_name }} {{ post.User.last_name }} : <v-chip
-                                class="ma-2 white--text" outlined color="primary">{{   post.message   }}</v-chip>
+
+                        <div class="font-weight-bold ml-8 mb-2">{{ post.User.first_name }} {{ post.User.last_name }} :
+                            <v-chip class="ma-2 white--text" outlined color="primary">{{ post.message }}</v-chip>
                         </div>
                         <div class="offset-md-1 mx-auto">
 
                         </div>
                         <v-divider></v-divider>
-                        
+
                         <div v-if="post.Comments.length">
                             <v-col>
                                 <v-btn icon color="black" @click="toggleActive(index); commentShow(post, post.id)">
@@ -107,36 +110,36 @@
                             </v-col>
                         </div>
                         <v-expand-transition>
-                            
+
                             <div v-show="show.includes(index)">
-                            
+
                                 <div v-for="comment in post.Comments" :key="comment.id"
                                     class="d-flex align-center pr-4 pl-4">
-                                    
+
                                     <v-card-text>
-                                        
+
                                         <span class="profil" @click="profil(comment.UserId)">
                                             <b>{{ comment.first_name }} {{ comment.last_name }} : </b>
                                         </span>
                                         <v-chip class="ma-2 white--text" color="primary">
                                             {{ comment.text }}</v-chip>
-                                            <v-spacer></v-spacer>
-                                            
+                                        <v-spacer></v-spacer>
+
                                     </v-card-text>
                                     <v-icon v-if="comment.UserId == UserId || userAdmin === true" class="cross"
                                         color="dark" @click="deleteComment(comment.id)">x</v-icon>
-                                        
+
                                 </div>
-                                
+
                             </div>
-                            
+
                         </v-expand-transition>
-                        
+
                         <div>
                             <v-col width="900" class="d-flex justify-end">
-                                <v-btn  @click="commentPost(post.id)" outlined class="primary--text">
+                                <v-btn @click="commentPost(post.id)" outlined class="primary--text">
                                     <v-icon>mdi-tooltip-edit-outline</v-icon>
-                                    
+
                                 </v-btn>
                             </v-col>
                         </div>
